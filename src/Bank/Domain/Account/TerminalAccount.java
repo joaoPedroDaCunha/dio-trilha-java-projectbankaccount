29a217e39bb153356509274c0407893fd93bc881
joaoPedroDaCunha <joaopedrodacunha.santos@gmail.com>
2024-10-05 22:30:34 -0300

    creat accont

--- src/Bank/Domain/Account/TerminalAccount.java
+++ src/Bank/Domain/Account/TerminalAccount.java
@@ -5,6 +5,38 @@ public class TerminalAccount {
     private int number ;
     private String agency;
     private String customerName;
-    private float saldo;
+    private float balance;
+
+    public TerminalAccount(){
+        
+    }
     
+    public int getNumber(){
+        return this.number;
+    }
+    public String getAgency(){
+        return this.agency;
+    }
+    public String getCustomerName(){
+        return this.customerName;
+    }
+    public float gatBalance(){
+        return this.balance;
+    }
+    public void setBalance(float balance){
+        this.balance = balance;
+    }
+    public void setCustomerName(String customerName){
+        this.customerName = customerName;
+    }
+    public void setAgency(String agency){
+        this.agency = agency;
+    }
+    public void setNumber(int number){
+        this.number = number;
+    }
+
+    public String toString(){
+        return "Olá "+customerName+", obrigado por criar uma conta no nosso banco, sua agencia é "+agency+", conta "+number+" e seu saldo "+balance+" ja esta disponivel para saque.";
+    }
 }
--- src/Terminal/Domain/Controller/Program.java
+++ src/Terminal/Domain/Controller/Program.java
@@ -1,9 +1,32 @@
 package Terminal.Domain.Controller;
 
+import java.util.Scanner;
+
+import Bank.Domain.Account.TerminalAccount;
+
 public class Program {
 
     public static void main(String [] args){
 
+        Scanner sc = new Scanner (System.in);
+        TerminalAccount Account = new TerminalAccount();
+
+        System.out.print("Digite o numero da Conta : ");
+        Account.setNumber(sc.nextInt());
+
+        System.out.print("Digite o numero da Agencia : ");
+        Account.setAgency(sc.next());
+        sc.nextLine();
+
+        System.out.print("Digite o seu Nome : ");
+        Account.setCustomerName(sc.nextLine());
+
+        System.out.print("Digite seu Saldo : ");
+        Account.setBalance(sc.nextFloat());
+
+        System.out.println(Account.toString());
+
+        sc.close();
     }
     
 }
